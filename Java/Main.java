d4a988693daeb01f8b15727ed497cbb71aace71a
Vivian Munguía <andy_munguia@hotmail.com>
2020-06-05 12:44:34 -0700

    Inicialización del repositorio con las clases Account, Car, Payment, Route y Main en los lenguajes Java, JS, PHP y Python

---
+++ Java/Main.java
@@ -0,0 +1,8 @@
+package Java;
+public class Main { //El nombre de la clase debe tener el mismo nombre del archivo
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        Car car = new Car();
+        car.license = "AMQ123";
+    }
+}
---
+++ Python/car.py
@@ -0,0 +1,7 @@
+class Car:
+    id = int
+    license = str
+    driver = str
+    passenger = int
+
+    
