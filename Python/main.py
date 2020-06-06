1846b7d6443f6308c090bafad57f80a94bb8a17f
Vivian Munguía <andy_munguia@hotmail.com>
2020-06-06 00:39:20 -0700

    Creación del método constructor en las clases account.py y car.py

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,10 @@
 from car import Car                             #car es el nombre del archivo #Car es el nombre de la clase
+from account import Account
+
 if __name__ == "__main__":
     print("Hola Mundo")
-    car = Car()                                        #no se necesita ni el tipo de dato ni la palabra new
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))       #no se necesita ni el tipo de dato ni la palabra new
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Martha Pérez"
-    print(vars(car2))
-
+    print(vars(car.driver))
     
