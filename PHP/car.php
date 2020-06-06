717d2aaff7411e42e4d8912788e2af3afd1f00b1
Vivian Munguía <andy_munguia@hotmail.com>
2020-06-06 02:40:01 -0700

    Creación del método constructor en las clases car.php y account.php

--- PHP/car.php
+++ PHP/car.php
@@ -1,8 +1,20 @@
 <?php
+require_once 'account.php';
 class Car {
     public $id;
     public $license;
     public $driver;
     public $passenger;
+
+    public function __construct($license, $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+
+    public function printDataCar() {
+        echo "<p> License: ". $this->license . "</p>";
+        echo "<p> Driver name: ". $this->driver->name . "</p>";
+        echo "<p> Driver document: ". $this->driver->document . "</p>";
+    }
 }
 ?>
