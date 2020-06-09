1204e1888d662e2fd2cc431bcd44d676bf6bde0b
Vivian Munguía <andy_munguia@hotmail.com>
2020-06-09 01:42:00 -0700

    Creación de las clases driver.py y user.py

---
+++ Python/driver.py
@@ -0,0 +1,11 @@
+from account import Account
+
+class Driver(Account):
+    id = int
+    name = str
+    document = str
+    email = str
+    password = str
+
+    def __init__(self, id, name, document, email, password):
+        super().__init__(id, name, document, email, password)
